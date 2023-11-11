7a90ecd0c651c0994ae1f82d0d83d78427ed1ded
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-12 01:10:38 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,25 @@ type Props = {};
 const page = (props: Props) => {
   const dispatch = useAppDispatch();
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h2 className='text-center text-emerald-500'>Home</h2>
-      <Modal
+    <main className='flex p-4'>
+      <div className='bg-blue-600 w-full h-40 rounded-lg flex flex-row'>
+        <div className='flex flex-col items-start justify-center sm:p-4 p-2 pl-6'>
+          <p className='text-white font-bold sm:text-3xl text-xl'>
+            Looking for a new opportunites?
+          </p>
+          <p className='text-white sm:text-md text-sm'>
+            Browse our latest job opening
+          </p>
+        </div>
+      </div>
+
+      {/* <Modal
         title='Modal'
         subTitle='this is subtitle modal'
         negitiveLabel='close'
         positiveLabel='submit'
       ></Modal>
-      <Button onClick={() => dispatch(openModal())}>Open Modal</Button>
+      <Button onClick={() => dispatch(openModal())}>Open Modal</Button> */}
     </main>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,17 @@
 'use client';
 import React, { ReactElement, ReactNode, useState, useCallback } from 'react';
 import Image from 'next/image';
-import { MessagesSquare, Bell, User2, X, Menu } from 'lucide-react';
+import {
+  MessagesSquare,
+  Bell,
+  User2,
+  X,
+  Menu,
+  ChevronLeftCircle,
+} from 'lucide-react';
 import Link from 'next/link';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 type Props = {};
 interface HoverableProps {
@@ -24,7 +33,7 @@ const Hoverable = ({ children, path, underLine }: HoverableProps) => {
   );
 };
 
-let middleLinks = [
+let navLinks = [
   {
     label: 'Find Job',
     path: '/',
@@ -41,12 +50,6 @@ let middleLinks = [
     underLine: true,
   },
 ];
-let rightLinks = [
-  { icon: <MessagesSquare />, text: 'Messages', path: '/', underLine: false },
-  { icon: <Bell />, text: 'Bell', path: '/', underLine: false },
-  { icon: <User2 />, text: 'User', path: '/', underLine: false },
-  { text: 'Employers/Post Job', path: '/', underLine: true },
-];
 
 const Navbar = (props: Props) => {
   const [toggleNavbar, setToggleNavbar] = useState<Boolean>(false);
@@ -55,47 +58,32 @@ const Navbar = (props: Props) => {
     setToggleNavbar(!toggleNavbar);
   }, [toggleNavbar]);
 
-  return (
-    <div className='flex flex-row p-4 justify-between'>
-      <div className='md:flex-[0.7] lg:flex-1'>
-        <Image
-          src={'/images/twitter_header_photo_1.png'}
-          alt='Logo'
-          width={100}
-          height={100}
-        />
-      </div>
-      <div className='flex-1 lg:block hidden'>
-        <ul className='flex flex-row gap-3 items-center justify-center'>
-          {middleLinks.map((link, index) => (
-            <Hoverable path={link.path} key={index} underLine={link.underLine}>
-              {link.label}
-            </Hoverable>
-          ))}
-        </ul>
-      </div>
-      <div className='flex-1 lg:block hidden '>
-        <ul className='flex flex-row justify-end gap-3 mr-4'>
-          {rightLinks.map((link, index) => (
-            <Hoverable key={index} path={link.path} underLine={link.underLine}>
-              {link.icon ? link.icon : link.text}
-            </Hoverable>
-          ))}
-        </ul>
-      </div>
+  const handleOutsideClick = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      e.stopPropagation();
+      if (e.currentTarget === e.target) {
+        if (toggleNavbar) {
+          toggleNavbarFn();
+        }
+      }
+    },
+    [toggleNavbar, toggleNavbarFn]
+  );
 
-      {/* mobile view */}
-      <div className='flex lg:hidden z-10' onClick={toggleNavbarFn}>
-        {toggleNavbar ? <X /> : <Menu />}
-      </div>
-      <div
-        className={`flex flex-col items-center justify-center lg:hidden absolute bg-red-600 h-screen w-[40%] top-0 ${
-          toggleNavbar ? 'right-0' : 'right-[-100%]'
-        } transition-all duration-200 ease-in-out`}
-      >
-        <div className='flex-1 flex items-center justify-center '>
-          <ul className='flex flex-col gap-3 items-center justify-center'>
-            {middleLinks.map((link, index) => (
+  return (
+    <div className='flex flex-row items-center justify-between overflow-hidden'>
+      <div className='container flex flex-row p-4 items-center justify-between overflow-hidden'>
+        <div className='md:flex-1 lg:flex-1'>
+          <Image
+            src={'/images/twitter_header_photo_1.png'}
+            alt='Logo'
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className='flex-1 lg:block hidden'>
+          <ul className='flex flex-row gap-3 items-center justify-end'>
+            {navLinks.map((link, index) => (
               <Hoverable
                 path={link.path}
                 key={index}
@@ -105,6 +93,54 @@ const Navbar = (props: Props) => {
               </Hoverable>
             ))}
           </ul>
+        </div>
+
+        {/* mobile view */}
+        <div className='flex lg:hidden z-10' onClick={toggleNavbarFn}>
+          {toggleNavbar ? <X /> : <Menu />}
+        </div>
+        <div
+          className={`fixed ${
+            toggleNavbar ? 'right-0' : 'right-[-120%]'
+          } top-0 z-50 flex h-[100vh] w-[100vw] flex-col items-end bg-gray-900/20 transition-all duration-500 ease-in-out`}
+          onClick={handleOutsideClick}
+        >
+          <div className='h-full w-full bg-white p-4 text-black dark:bg-dark dark:text-white md:w-2/4'>
+            <div className='flex w-full flex-row items-center justify-between'>
+              <button
+                onClick={toggleNavbarFn}
+                className='my-6 cursor-pointer text-black dark:text-white'
+              >
+                <ChevronLeftCircle />
+              </button>
+              <Image
+                src='/images/twitter_header_photo_1.png'
+                alt='none'
+                width={50}
+                height={40}
+              />
+            </div>
+            <div className='order-3  flex w-full flex-1 flex-row items-center justify-center gap-2 md:order-none'>
+              <Input
+                type='text'
+                className='border-slate-700 bg-white focus:ring-offset-0 focus-visible:ring-offset-0 dark:bg-dark '
+              />
+              <Button>Search</Button>
+            </div>
+            <div className='flex-1 flex items-center justify-center mt-5'>
+              <ul className='flex flex-col gap-3 items-center justify-center'>
+                {navLinks.map((link, index) => (
+                  <Hoverable
+                    path={link.path}
+                    key={index}
+                    underLine={link.underLine}
+                  >
+                    {link.label}
+                  </Hoverable>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
