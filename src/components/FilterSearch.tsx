15076c86d58a6732618e5ce1898a00ab149cf49c
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-17 18:33:11 +0530

    added static.data.ts

--- src/components/FilterSearch.tsx
+++ src/components/FilterSearch.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Input, Button } from '@/components/';
 import Selection from './Selection';
+import { jobTypes } from '@/utils/Static.data';
 
 type Props = {};
-
-const jobTypes = [
-  { label: 'Full-time', value: 'Full-time' },
-  { label: 'Remote', value: 'Remote' },
-  { label: 'Part-time', value: 'Part-time' },
-  { label: 'Freelance', value: 'Freelance' },
-  { label: 'Contract', value: 'Contract' },
-  { label: 'Temporary', value: 'Temporary' },
-  { label: 'Internship', value: 'Internship' },
-  { label: 'Flexible Schedule', value: 'Flexible Schedule' },
-];
 
 const FilterSearch = (props: Props) => {
   return (
---
+++ src/utils/Static.data.ts
@@ -0,0 +1,10 @@
+export const jobTypes = [
+  { label: 'Full-time', value: 'Full-time' },
+  { label: 'Remote', value: 'Remote' },
+  { label: 'Part-time', value: 'Part-time' },
+  { label: 'Freelance', value: 'Freelance' },
+  { label: 'Contract', value: 'Contract' },
+  { label: 'Temporary', value: 'Temporary' },
+  { label: 'Internship', value: 'Internship' },
+  { label: 'Flexible Schedule', value: 'Flexible Schedule' },
+];
